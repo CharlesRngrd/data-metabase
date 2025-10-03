a0d57844a7e2cf35f908e5dbc127b40d763ff4bd
charles <charles.rangheard@hotmail.fr>
2025-10-03 09:52:45 +0200

    Génération de la carte en python
    Quelques modifications sont nécessaires par rapport à la carte de data.gouv

---
+++ README.md
@@ -0,0 +1,10 @@
+# Installation
+
+## Chargement de la carte des circonsriptions dans Metabse :
+
+- Executer `python map.py`.
+- Publier la carte sur GitHub.
+- Charger la carte dans Metabase à partir de l'URL de la carte sur GitHub.
+
+Attention GitHub exige une 200 et non une 302.<br>
+Voici l'URL à utiliser : https://raw.githubusercontent.com/CharlesRngrd/data_demo/refs/heads/main/map.geojson
---
+++ map.py
@@ -0,0 +1,26 @@
+import json
+import requests
+
+# URL retrieved from :
+# https://www.data.gouv.fr/datasets/contours-geographiques-des-circonscriptions-legislatives
+map_json = requests.get(
+    "https://www.data.gouv.fr/api/1/datasets/r/8b681b69-739c-47eb-a96b-06e8e2d8dc08"
+).json()
+
+
+# DOM-TOM codeDepartement always starts with Z :
+map_json["features"][:] = [
+    f
+    for f in map_json["features"]
+    if not f["properties"]["codeDepartement"].startswith("Z")
+]
+
+for feature in map_json["features"]:
+    property = feature["properties"]
+
+    property["labelCirconscription"] = (
+        f"{property['nomDepartement']} - {property['nomCirconscription']}"
+    )
+
+with open("map.geojson", "w") as json_file:
+    json.dump(map_json, json_file)
