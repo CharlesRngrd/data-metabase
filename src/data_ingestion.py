ae264f17d8e977ab7def7c02583e86c3e105b663
charles <charles.rangheard@hotmail.fr>
2025-10-08 17:16:03 +0200

    Ingestion des données de 2022 + Correction d'un parti

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -3,34 +3,35 @@ import requests
 import pandas as pd
 
 # URL retrieved from :
+# https://www.data.gouv.fr/datasets/elections-legislatives-des-12-et-19-juin-2022-resultats-definitifs-du-premier-tour/
+# https://www.data.gouv.fr/datasets/elections-legislatives-des-12-et-19-juin-2022-resultats-definitifs-du-second-tour/
 # https://www.data.gouv.fr/datasets/elections-legislatives-des-30-juin-et-7-juillet-2024-resultats-definitifs-du-1er-tour/
 # https://www.data.gouv.fr/datasets/elections-legislatives-des-30-juin-et-7-juillet-2024-resultats-definitifs-du-2nd-tour
 
-tours = [
-    "5163f2e3-1362-4c35-89a0-1934bb74f2d9",
-    "1050324f-a0ce-4a06-94c5-e2f6805fdb74",
-]
+elections = {
+    "election_2022_tour_1": "a5e0199c-c98b-43b5-8a9f-e8a8b4133b2e",
+    "election_2022_tour_2": "3ce8fa1a-a0d3-4012-b9c0-10c480cefeda",
+    "election_2024_tour_1": "5163f2e3-1362-4c35-89a0-1934bb74f2d9",
+    "election_2024_tour_2": "1050324f-a0ce-4a06-94c5-e2f6805fdb74",
+}
 
 
-def get_data_election(label, tour):
-    next_url = f"https://tabular-api.data.gouv.fr/api/resources/{tours[tour]}/data/?page_size=200"
-    election_2024_list = []
+def get_data_election(label, election_id):
+    next_url = f"https://tabular-api.data.gouv.fr/api/resources/{election_id}/data/?page_size=200"
+    election_list = []
 
     while next_url:
-        election_2024_json = requests.get(next_url).json()
+        response = requests.get(next_url)
+        response.raise_for_status()
 
-        next_url = election_2024_json["links"]["next"]
-
-        election_2024_list.extend(election_2024_json["data"])
-
-    df = pd.DataFrame(election_2024_list)
-
-    print(f"Nombre de circonscriptions : {len(df)}")
+        next_url = response.json()["links"]["next"]
+        election_list.extend(response.json()["data"])
 
     with duckdb.connect("assets/data.duckdb") as con:
+        con.register("df", pd.DataFrame(election_list))
         con.execute(f"CREATE OR REPLACE TABLE {label} AS SELECT * FROM df")
         con.table(label).show()
 
 
-get_data_election("election_2024_tour_1", 0)
-get_data_election("election_2024_tour_2", 1)
+for dataset, election_id in elections.items():
+    get_data_election(dataset, election_id)
--- src/data_transformation.py
+++ src/data_transformation.py
@@ -1,105 +1,189 @@
 import duckdb
 import pandas as pd
+from pandas.api.types import is_numeric_dtype
 
 
 with duckdb.connect("assets/data.duckdb") as con:
-    df_tour_1 = con.sql("SELECT * FROM election_2024_tour_1").fetchdf()
-    df_tour_2 = con.sql("SELECT * FROM election_2024_tour_2").fetchdf()
-
     columns = {
+        # Format 2022
+        "Code du département": "departement_code",
+        "Libellé du département": "departement_nom",
+        "Code de la circonscription": "circonscription_code",
+        "Libellé de la circonscription": "circonscription_nom",
+        "% Abs/Ins": "abstention_pourcentage",
+        "Exprimés": "exprime",
+        # Format 2024
         "Code département": "departement_code",
         "Libellé département": "departement_nom",
         "Code circonscription législative": "circonscription_code",
         "Libellé circonscription législative": "circonscription_nom",
         "% Abstentions": "abstention_pourcentage",
-        "Exprimés": "exprime",
     }
 
     label_parti = {
         # Rassemblement National
         "RN": "Rassemblement National",
-        "UDI": "Rassemblement National",
         "UXD": "Rassemblement National",
         "EXD": "Rassemblement National",
         # Les Républicains
         "LR": "Les Républicains",
         "DVD": "Les Républicains",
+        # Ensemble
+        "ENS": "Ensemble",
+        "HOR": "Ensemble",
+        "UDI": "Ensemble",
+        "DVC": "Ensemble",
         # Nouveau Front Populaire
         "UG": "Nouveau Front Populaire",
         "SOC": "Nouveau Front Populaire",
         "ECO": "Nouveau Front Populaire",
         "DVG": "Nouveau Front Populaire",
-        # Ensemble
-        "ENS": "Ensemble",
-        "HOR": "Ensemble",
-        "DVC": "Ensemble",
         # Autre
         "REG": "Régionaliste",
         "DIV": "Divers",
     }
 
-    def clean_pourcentage(serie):
-        for old, new in ({"%": "", ",": "."}).items():
-            serie = serie.str.replace(old, new)
-
-        return pd.to_numeric(serie)
-
     def generate_columns_candidat(numero):
         return {
+            # Format 2022
+            f"Nuance {numero}": "candidat_parti",
+            f"Nom {numero}": "candidat_nom",
+            f"Prénom {numero}": "candidat_prenom",
+            f"Sexe {numero}": "candidat_sexe",
+            f"Voix {numero}": "candidat_voix",
+            f"% Voix/Exp {numero}": "candidat_voix_pourcentage",
+            # Format 2024
             f"Nuance candidat {numero}": "candidat_parti",
             f"Nom candidat {numero}": "candidat_nom",
             f"Prénom candidat {numero}": "candidat_prenom",
             f"Sexe candidat {numero}": "candidat_sexe",
-            f"Voix {numero}": "candidat_voix",
             f"% Voix/exprimés {numero}": "candidat_voix_pourcentage",
         }
 
-    def reshape_data(df, columns):
+    def load_dataframes():
+        elections = ["2022_tour_1", "2022_tour_2", "2024_tour_1", "2024_tour_2"]
+
+        return {
+            f"df_{election}": con.sql(f"SELECT * FROM election_{election}").fetchdf()
+            for election in elections
+        }
+
+    def pivot_data(df, columns):
         df_final = pd.DataFrame()
 
-        for numero in range(20):
+        for numero in range(100):
             columns_tmp = columns.copy()
             columns_tmp.update(generate_columns_candidat(numero + 1))
 
-            if f"Nom candidat {numero + 1}" not in df.columns:
+            if not any(col for col in df.columns if col.endswith(str(numero + 1))):
                 continue
 
             df_tmp = df.copy()
-            df_tmp = df_tmp[columns_tmp.keys()]
+            df_tmp = df_tmp[df_tmp.columns.intersection(columns_tmp.keys())]
             df_tmp = df_tmp.rename(columns=columns_tmp)
 
             df_final = pd.concat([df_final, df_tmp])
+        
+        return df_final
 
-        df_final["candidat_denomination"] = (
-            df_final["candidat_nom"] + " " + df_final["candidat_prenom"]
+    def clean_common(df):
+        df["candidat_denomination"] = (
+            df["candidat_nom"] + " " + df["candidat_prenom"]
         )
 
-        df_final["departement_code"] = df_final["departement_code"].str.pad(
+        df["departement_code"] = df["departement_code"].str.pad(
             width=2, side="left", fillchar="0"
         )
 
-        df_final["circonscription_code"] = df_final["circonscription_code"].str.pad(
+        df["circonscription_code"] = df["circonscription_code"].str.pad(
             width=4, side="left", fillchar="0"
         )
 
-        return df_final
+        return df
 
-    df_tour_1 = reshape_data(df_tour_1, columns)
-    df_tour_2 = reshape_data(df_tour_2, columns)
+    def clean_pourcentage(serie):
+        for old, new in ({"%": "", ",": "."}).items():
+            serie = serie.astype(str).str.replace(old, new)
 
-    df_tour_2_complement = df_tour_1.loc[
-        ~df_tour_1["circonscription_code"].isin(df_tour_2["circonscription_code"]), :
-    ]
+        return pd.to_numeric(serie)
 
-    df = pd.concat([df_tour_2, df_tour_2_complement])
+    def preprocess_2022_format(df):
+        to_remane_cols = [
+            "N°Panneau",
+            "Sexe",
+            "Nom",
+            "Prénom",
+            "Nuance",
+            "Voix",
+            "% Voix/Ins",
+            "% Voix/Exp",
+            "Sièges",
+        ]
+
+        for col in to_remane_cols:
+            df = df.rename(columns={col: f"{col} 1"})
+
+        n_unnamed = len([c for c in df.columns if c.startswith("Unnamed")])
+        n_groups = n_unnamed // len(to_remane_cols)
+
+        new_cols = [c for c in df.columns if not c.startswith("Unnamed")]
+        for i in range(2, n_groups + 2):
+            new_cols += [f"{col} {i}" for col in to_remane_cols]
+
+        df.columns = new_cols
+
+        return df
+
+    def clean_2022_values(df):
+        df["circonscription_code"] = df["departement_code"] + df["circonscription_code"].astype(str).str.pad(
+            width=2, side="left", fillchar="0"
+        )
+
+        return df
+
+    def merge_tours(df_tour_1, df_tour_2, year):
+        df_tour_2_complement = df_tour_1.loc[
+            ~df_tour_1["circonscription_code"].isin(df_tour_2["circonscription_code"]), :
+        ]
+
+        df = pd.concat([df_tour_2, df_tour_2_complement])
+
+        df["annee"] = year
+
+        return df
+
+    dfs = load_dataframes()
+
+    dfs["df_2022_tour_1"]["Unnamed fake"] = ""
+
+    dfs["df_2022_tour_1"] = preprocess_2022_format(dfs["df_2022_tour_1"])
+    dfs["df_2022_tour_2"] = preprocess_2022_format(dfs["df_2022_tour_2"])
+
+    dfs["df_2022_tour_1"] = pivot_data(dfs["df_2022_tour_1"], columns)
+    dfs["df_2022_tour_2"] = pivot_data(dfs["df_2022_tour_2"], columns)
+    dfs["df_2024_tour_1"] = pivot_data(dfs["df_2024_tour_1"], columns)
+    dfs["df_2024_tour_2"] = pivot_data(dfs["df_2024_tour_2"], columns)
+
+    dfs["df_2022_tour_1"] = clean_2022_values(dfs["df_2022_tour_1"])
+    dfs["df_2022_tour_2"] = clean_2022_values(dfs["df_2022_tour_2"])
+
+    dfs["df_2022_tour_1"] = clean_common(dfs["df_2022_tour_1"])
+    dfs["df_2022_tour_2"] = clean_common(dfs["df_2022_tour_2"])
+    dfs["df_2024_tour_1"] = clean_common(dfs["df_2024_tour_1"])
+    dfs["df_2024_tour_2"] = clean_common(dfs["df_2024_tour_2"])
+
+    df_2022 = merge_tours(dfs["df_2022_tour_1"], dfs["df_2022_tour_2"], 2022)
+    df_2024 = merge_tours(dfs["df_2024_tour_1"], dfs["df_2024_tour_2"], 2024)
+
+    df = pd.concat([df_2022, df_2024])
 
     df["candidat_parti"] = df["candidat_parti"].replace(label_parti)
 
     df = df.sort_values(
-        ["circonscription_code", "candidat_voix"], ascending=[True, False]
+        ["annee", "circonscription_code", "candidat_voix"], ascending=[True, True, False]
     )
 
-    df["candidat_rang"] = df.groupby(["circonscription_code"])["candidat_voix"].rank(
+    df["candidat_rang"] = df.groupby(["annee", "circonscription_code"])["candidat_voix"].rank(
         method="min", ascending=False
     )
 
@@ -108,5 +192,7 @@ with duckdb.connect("assets/data.duckdb") as con:
     df["abstention_pourcentage"] = clean_pourcentage(df["abstention_pourcentage"])
     df["candidat_voix_pourcentage"] = clean_pourcentage(df["candidat_voix_pourcentage"])
 
-    con.execute("CREATE OR REPLACE TABLE election_2024_mart AS SELECT * FROM df")
-    con.table("election_2024_mart").show()
+    print(df[df["annee"] == 2022]["candidat_voix_pourcentage"])
+
+    con.execute("CREATE OR REPLACE TABLE election_mart AS SELECT * FROM df")
+    con.table("election_mart").show()
