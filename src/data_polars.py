77dfb4d9d7fd88bb09974bec9d314ef2b3c0188a
charles <charles.rangheard@hotmail.fr>
2025-10-22 10:42:37 +0200

    Ajout du script Polars + Test vs Pandas

--- src/data_polars.py
+++ src/data_polars.py
@@ -2,7 +2,7 @@ import time
 from typing import Dict
 
 import duckdb
-import pandas as pd
+import polars as pl
 
 
 start_time = time.time()
@@ -67,16 +67,16 @@ label_parti = {
 }
 
 
-def load_dataframes(df_name: str) -> Dict[str, pd.DataFrame]:
+def load_dataframes(df_name: str) -> Dict[str, pl.DataFrame]:
     """Charge une table depuis DuckDB"""
 
-    return con.sql(f"SELECT * FROM election_{df_name}").fetchdf()
+    return con.sql(f"SELECT * FROM election_{df_name}").pl()
 
 
-def pivot_data(df: pd.DataFrame, columns: Dict[str, str]) -> pd.DataFrame:
+def pivot_data(df: pl.DataFrame, columns: Dict[str, str]) -> pl.DataFrame:
     """Transforme un dataframe électoral large en format long par candidat"""
 
-    df_final = pd.DataFrame()
+    df_final = pl.DataFrame()
 
     for numero in range(100):
         columns_tmp = columns.copy()
@@ -88,36 +88,55 @@ def pivot_data(df: pd.DataFrame, columns: Dict[str, str]) -> pd.DataFrame:
         if not any(col.endswith(str(numero + 1)) for col in df.columns):
             continue
 
-        df_tmp = df.copy()
-        df_tmp = df_tmp[df.columns.intersection(columns_tmp.keys())]
-        df_tmp = df_tmp.rename(columns=columns_tmp)
+        df_tmp = df.select([col for col in df.columns if col in columns_tmp.keys()])
+        df_tmp = df_tmp.rename(columns_tmp, strict=False)
 
-        df_final = pd.concat([df_final, df_tmp])
+        df_tmp = df_tmp.with_columns(pl.col("candidat_voix").cast(pl.Int64))
+
+        df_final = pl.concat([df_final, df_tmp])
 
     return df_final
 
 
-def clean_common(df: pd.DataFrame) -> pd.DataFrame:
+def clean_common(df: pl.DataFrame) -> pl.DataFrame:
     """Nettoyage commun à tous les jeux de données"""
 
-    df["candidat_denomination"] = df["candidat_nom"] + " " + df["candidat_prenom"]
+    df = df.with_columns(
+        (pl.col("candidat_nom") + " " + pl.col("candidat_prenom")).alias(
+            "candidat_denomination"
+        )
+    )
 
-    df["departement_code"] = df["departement_code"].str.zfill(2)
-    df["circonscription_code"] = df["circonscription_code"].str.zfill(4)
+    df = df.with_columns(
+        pl.col("departement_code").cast(pl.Utf8).str.zfill(2).alias("departement_code")
+    )
+
+    df = df.with_columns(
+        pl.col("circonscription_code")
+        .cast(pl.Utf8)
+        .str.zfill(4)
+        .alias("circonscription_code")
+    )
+
+    df = clean_pourcentage(df, "abstention_pourcentage")
+    df = clean_pourcentage(df, "candidat_voix_pourcentage")
 
     return df
 
 
-def clean_pourcentage(serie: pd.Series) -> pd.Series:
+def clean_pourcentage(df: pl.DataFrame, column_name: str) -> pl.DataFrame:
     """Convertie une valeur du type '10,0%' en 10.0"""
 
-    for old, new in ({"%": "", ",": "."}).items():
-        serie = serie.astype(str).str.replace(old, new)
+    return df.with_columns(
+        pl.col(column_name)
+        .cast(pl.Utf8)
+        .str.replace_all("%", "")
+        .str.replace_all(",", ".")
+        .cast(pl.Float64)
+    )
 
-    return pd.to_numeric(serie)
 
-
-def preprocess_2022_format(df: pd.DataFrame) -> pd.DataFrame:
+def preprocess_2022_format(df: pl.DataFrame) -> pl.DataFrame:
     """Nettoyage spécifique aux datasets de 2022"""
 
     to_remane_cols = [
@@ -132,8 +151,7 @@ def preprocess_2022_format(df: pd.DataFrame) -> pd.DataFrame:
         "Sièges",
     ]
 
-    for col in to_remane_cols:
-        df = df.rename(columns={col: f"{col} 1"})
+    df = df.rename({col: f"{col} 1" for col in to_remane_cols})
 
     n_unnamed = len([c for c in df.columns if c.startswith("Unnamed")])
     n_groups = n_unnamed // len(to_remane_cols)
@@ -147,38 +165,51 @@ def preprocess_2022_format(df: pd.DataFrame) -> pd.DataFrame:
     return df
 
 
-def postprocess_2022_values(df: pd.DataFrame) -> pd.DataFrame:
+def postprocess_2022_values(df: pl.DataFrame) -> pl.DataFrame:
     """Nettoyage spécifique aux datasets de 2022"""
 
-    df["circonscription_code"] = df["circonscription_code"].astype(str).str.zfill(2)
-    df["circonscription_code"] = df["departement_code"] + df["circonscription_code"]
+    df = df.with_columns(
+        [
+            pl.col("circonscription_code")
+            .cast(pl.Utf8)
+            .str.zfill(2)
+            .alias("circonscription_code"),
+        ]
+    )
+
+    df = df.with_columns(
+        (pl.col("departement_code") + pl.col("circonscription_code")).alias(
+            "circonscription_code"
+        )
+    )
 
     return df
 
 
 def merge_tour_1_2(
-    df_tour_1: pd.DataFrame, df_tour_2: pd.DataFrame, year: int
-) -> pd.DataFrame:
+    df_tour_1: pl.DataFrame, df_tour_2: pl.DataFrame, year: int
+) -> pl.DataFrame:
     """Concatenation du 1er et 2e tour"""
 
-    df_extra = df_tour_1.loc[
-        ~df_tour_1["circonscription_code"].isin(df_tour_2["circonscription_code"]),
-        :,
-    ]
+    df_extra = df_tour_1.filter(
+        ~pl.col("circonscription_code").is_in(df_tour_2["circonscription_code"].to_list())
+    )
 
-    df = pd.concat([df_tour_2, df_extra])
+    df = pl.concat([df_tour_2, df_extra])
 
-    df["annee"] = year
+    df = df.with_columns(pl.lit(year).alias("annee"))
 
     return df
 
 
 with duckdb.connect("assets/data.duckdb") as con:
-    dfs: Dict[str, pd.DataFrame] = dict()
+    dfs: Dict[str, pl.DataFrame] = dict()
     for df in ["2022_tour_1", "2022_tour_2", "2024_tour_1", "2024_tour_2"]:
         dfs[f"df_{df}"] = load_dataframes(df)
 
-    dfs["df_2022_tour_1"]["Unnamed fake"] = ""
+    dfs["df_2022_tour_1"] = dfs["df_2022_tour_1"].with_columns(
+        pl.lit("").alias("Unnamed fake")
+    )
 
     for df in ["df_2022_tour_1", "df_2022_tour_2"]:
         dfs[df] = preprocess_2022_format(dfs[df])
@@ -195,33 +226,42 @@ with duckdb.connect("assets/data.duckdb") as con:
     df_2022 = merge_tour_1_2(dfs["df_2022_tour_1"], dfs["df_2022_tour_2"], 2022)
     df_2024 = merge_tour_1_2(dfs["df_2024_tour_1"], dfs["df_2024_tour_2"], 2024)
 
-    df = pd.concat([df_2022, df_2024])
+    # Need this technical reshape because columns are not in the same order.
+    df_2024 = df_2024.select([c for c in df_2022.columns if c in df_2022.columns])
 
-    df["candidat_parti"] = df["candidat_parti"].replace(label_parti)
+    df = pl.concat([df_2022, df_2024])
 
-    df = df[df["candidat_voix_pourcentage"].notna()]
-
-    df["abstention_pourcentage"] = clean_pourcentage(df["abstention_pourcentage"])
-    df["candidat_voix_pourcentage"] = clean_pourcentage(df["candidat_voix_pourcentage"])
-
-    df = df[df["candidat_voix_pourcentage"] >= 25]
-
-    df = df.sort_values(
-        ["annee", "circonscription_code", "candidat_voix"],
-        ascending=[True, True, False],
+    df = df.with_columns(
+        pl.col("candidat_parti").replace(label_parti).alias("candidat_parti")
     )
 
-    grouped = df.groupby(["annee", "circonscription_code"])["candidat_voix_pourcentage"]
+    df = df.filter(pl.col("candidat_voix_pourcentage").is_not_nan())
+    df = df.filter(pl.col("candidat_voix_pourcentage") >= 25)
 
-    df["candidat_rang"] = grouped.rank(method="min", ascending=False)
-    df["candidat_rang_1_score"] = grouped.transform("max")
+    df = df.sort(
+        by=["annee", "circonscription_code", "candidat_voix"],
+        descending=[False, False, True],
+    )
 
-    df["candidat_rang_1_ecart"] = (
-        df["candidat_voix_pourcentage"] - df["candidat_rang_1_score"]
-    ).round(2)
+    df = df.with_columns(
+        pl.col("candidat_voix_pourcentage")
+        .rank(method="min", descending=True)
+        .over("annee", "circonscription_code")
+        .alias("candidat_rang"),
+        pl.col("candidat_voix_pourcentage")
+        .max()
+        .over("annee", "circonscription_code")
+        .alias("candidat_rang_1_score"),
+    )
 
-    con.execute("CREATE OR REPLACE TABLE election_mart AS SELECT * FROM df")
-    con.table("election_mart").show()
+    df = df.with_columns(
+        (pl.col("candidat_voix_pourcentage") - pl.col("candidat_rang_1_score"))
+        .round(2)
+        .alias("candidat_rang_1_ecart")
+    )
+
+    con.execute("CREATE OR REPLACE TABLE election_mart_polars AS SELECT * FROM df")
+    con.table("election_mart_polars").show()
 
 end_time = time.time()
 
--- src/data_test.py
+++ src/data_test.py
@@ -9,8 +9,10 @@ with duckdb.connect("assets/data.duckdb") as con:
     df_spark = con.sql("SELECT * FROM election_mart_spark").fetchdf()
     print(df_spark.shape)
 
-    df_test = pd.concat([df_pandas, df_spark])
+    df_polars = con.sql("SELECT * FROM election_mart_polars").fetchdf()
+    print(df_polars.shape)
+
+    df_test = pd.concat([df_pandas, df_spark, df_polars])
     print(df_test.shape)
 
     df_test = df_test.drop_duplicates(keep=False)
-    print(df_test.shape)
